c449a084d388cd3b34a05f0045a12f52b70a2e0a
Edinyanga O. Ottoho <elzucky@gmail.com>
2020-04-08 14:26:58 +0100

    added insert feature

--- js/get-tables.js
+++ js/get-tables.js
@@ -1,3 +1,6 @@
+function _(elem) {
+	return document.getElementById(elem);
+}
 var upd = document.querySelectorAll(".update");
 var del = document.querySelectorAll(".delete");
 
@@ -60,6 +63,51 @@ function updateRow(x, y) {
 		if (this.readyState == 4 && this.status == 200) {
 			if (this.responseText != "error") {
 				alert("Successfully updated row value(s)!");
+			}
+			else {
+				alert("An error occured!");
+			}
+		}
+	}
+	xhttp.open("POST", "config.php", true);
+	xhttp.send(formdata);
+}
+
+var values_to_add = new Array();
+
+_("insert_pop").onclick = function() {
+	$('.ui.modal')
+	  .modal('show')
+	;
+	values_to_add = [];
+	var tab_elem = document.querySelectorAll(".tab_titles");
+	var cnt = tab_elem.length;
+	for (i = 0; i < cnt; i++) {
+		_("title_tab").innerHTML += '<th>'+ tab_elem[i].innerHTML +'</th>';
+		_("insert_tab").innerHTML += '<td><input class="input_fields"></td>';
+	}
+}
+_("insert").onclick = function() {
+	var elems = document.querySelectorAll(".input_fields");
+	for (i = 0; i < elems.length; i++) {
+		values_to_add.push(elems[i].value);
+	}
+	insertRow(values_to_add);
+}
+function insertRow(arr) {
+	var formdata = new FormData();
+	formdata.append("insert", arr);
+	var xhttp;
+	if (XMLHttpRequest) {
+		xhttp = new XMLHttpRequest();
+	}
+	else {
+		xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			if (this.responseText != "error") {
+				location.reload(1);
 			}
 			else {
 				alert("An error occured!");
